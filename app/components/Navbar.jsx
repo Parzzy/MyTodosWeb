7d1d8995e6cbcffbdf2422cddd6ca35c2c6aef04
Parzzy <parsaghazi8@gmail.com>
2024-10-08 20:17:39 +0300

    Dashboard button removed.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,18 +2,12 @@ import Link from "next/link";
 import DarkMode from "./DarkMode";
 export default function Navbar() {
   return (
-    <nav className="gap-3 md:gap-6 flex items-center justify-center md:items-center md:justify-center w-full p-3 md:w-1/2 md:mx-auto bg-neutral-300 dark:bg-neutral-900 rounded-3xl">
+    <nav className="gap-3 md:gap-6 flex items-center justify-center md:items-center md:justify-center w-full p-3 md:w-1/2 md:mx-auto bg-neutral-200 dark:bg-neutral-900 rounded-3xl">
       <Link
         href="/"
         className="dm-serif-text-regular md:p-2 text-xl md:text-3xl hover:bg-neutral-400 dark:hover:bg-neutral-900 rounded"
       >
         MyTodos
-      </Link>
-      <Link
-        href="/"
-        className="rounded md:p-2 hover:bg-neutral-400 dark:hover:bg-neutral-900"
-      >
-        <h1 className="text-xs md:text-lg">Dashboard</h1>
       </Link>
       <Link
         href="/completed-tasks"
