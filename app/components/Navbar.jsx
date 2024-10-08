7ec4dcfb58184a043dfdced69a8ec73d5524f757
Parzzy <parsaghazi8@gmail.com>
2024-10-08 17:57:25 +0300

    Title changes for testing.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="sticky top-full md:top-1 flex items-center justify-center md:items-center md:justify-center md:w-1/2 md:mx-auto md:p-2 p-2 bg-neutral-300 dark:bg-neutral-800 rounded-3xl mb-2 mx-2">
       <Link href="/" className="dm-serif-text-regular text-xl md:text-3xl p-2 hover:bg-neutral-400 dark:hover:bg-neutral-900 rounded">
-        MyTodos
+        MyTodosWeb
       </Link>
       <Link href="/" className="p-2 rounded hover:bg-neutral-400 dark:hover:bg-neutral-900">
         <h1 className="text-sm">Dashboard</h1>
